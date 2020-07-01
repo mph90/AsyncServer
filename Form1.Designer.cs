35c2287471d4b897bf7a76c59d393c2addd705f6
mph90 <mphall@protonmail.com>
2020-07-01 16:08:43 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AsyncServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(12, 29);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 2;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 55);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 265);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(179, 20);
+            this.textBoxMessage.TabIndex = 8;
+            this.textBoxMessage.TextChanged += new System.EventHandler(this.textBoxMessage_TextChanged);
+            this.textBoxMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMessage_KeyPress);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(197, 263);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 9;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Port";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 173);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 300);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsyncServer
+{
+    public partial class Form1 : Form
+    {
+        List<Socket> AllSockets = new List<Socket>();
+        Socket socket;
+        TcpListener listener;
+        byte[] buffer = new byte[256];
+        int port;
+        string message;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            textBoxPort.Text = "1001";
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            port = int.Parse(textBoxPort.Text);
+            Listen(port);
+        }
+
+        private void Listen(int port)
+        {
+            try
+            {
+                // Listen for input coming from any IP address on specified port
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                
+                // Create an event handler for dealing with incoming connections
+                listener.BeginAcceptTcpClient(new AsyncCallback(AcceptIncomingConnection), listener);
+            }
+            catch (Exception ex)
+            {
+                // Update display to show error message
+                listBox1.Items.Add("Socket connection error:\n" + ex.ToString());
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                listBox1.SelectedIndex = -1;
+            }
+        }
+
+        private void AcceptIncomingConnection(IAsyncResult info) // CALLBACK
+        {
+            // Which listener is this using?
+            TcpListener l = (TcpListener)info.AsyncState;
+            
+            // Accept incoming socket connection
+            Socket socket;
+            socket = l.EndAcceptSocket(info);
+           
+            // Set up an event handler for receiving messages
+            AllSockets.Add(socket);
+            foreach (Socket i in AllSockets)
+            {
+                Receive(socket);
+            }
+            
+            // Re-listen!
+            listener.BeginAcceptTcpClient(new AsyncCallback(AcceptIncomingConnection), listener);
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            SendInput();
+        }
+
+        private void textBoxMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                SendInput();
+            }
+        }
+
+        private void SendInput()
+        {
+            string displayMessage = "Server: " + textBoxMessage.Text;
+            listBox1.Items.Add(displayMessage);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.SelectedIndex = -1;
+
+            foreach (Socket i in AllSockets)
+            {
+                Transmit(i, displayMessage);
+            }
+
+            textBoxMessage.Text = String.Empty;
+        }
+
+        private void Transmit(Socket s, string text)
+        {
+            try
+            {
+                // Prepare message
+                byte[] messageBytes = Encoding.ASCII.GetBytes(text);
+                // Send it
+                s.BeginSend(messageBytes, 0, messageBytes.Length, 0, new AsyncCallback(TransmitHandler), s);
+            }
+            catch (Exception) {}
+        }
+
+        private void TransmitHandler(IAsyncResult info) // CALLBACK
+        {
+            // Which socket is this using?
+            Socket s = (Socket)info.AsyncState;
+            int bytesSent = s.EndSend(info);
+        }
+
+        private void Receive(Socket s)
+        {
+            s.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None,
+            ReceiveHandler, s);
+        }
+
+        private void ReceiveHandler(IAsyncResult info) // CALLBACK
+        {
+            try
+            {
+                // Which socket is this using?
+                Socket s = (Socket)info.AsyncState;
+                // Read message
+                int numBytesReceived = s.EndReceive(info);
+                message = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
+                // Update display
+                listBox1.Items.Add(message);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                listBox1.SelectedIndex = -1;
+                // Reset the event handler for new incoming messages on socket s
+                Receive(s);
+            }
+            catch (Exception) {}
+            
+            // Send the message to all connected clients
+            foreach (Socket n in AllSockets)
+            {
+                Transmit(n, message);
+            }
+        }
+
+        private void textBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
